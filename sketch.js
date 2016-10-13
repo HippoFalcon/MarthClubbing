039e5893a9fea261f59c3d1947f54252436ea38f
Brendan Hoppe <277467@bwls.net>
2016-10-13 18:29:59 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,9 +4,7 @@ function setup() {
 
 function draw() {
   ellipse( mouseX , mouseY , mouseX , mouseY  );
-  fill( 0 , 0 , 255 );
+  fill( 9 , 133, 255 );
   noStroke();
-  rectangle( mouseX+40 , mouseY+30 , mouseX-40 , mouseY+30, mouseX , mouseY-40 , 6 , 7);
-  fill(  0 , 255 , 0 );
-  background(255 , 0 , 0 , 40 );
+  background(255 , 73 , 43 , 40 );
 }
