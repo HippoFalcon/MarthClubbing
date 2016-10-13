d419143a21ff08174dafa0e9fbc1f6e456629e40
HippoFalcon <277467@bwls.net>
2016-10-13 16:49:46 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ function draw() {
   ellipse( mouseX , mouseY , mouseX , mouseY  );
   fill( 0 , 0 , 255 );
   noStroke();
-  rectangle( mouseX+40 , mouseY+30 , mouseX-40 , mouseY+30, mouseX , mouseY-40);
+  rectangle( mouseX+40 , mouseY+30 , mouseX-40 , mouseY+30, mouseX , mouseY-40 , 6 , 7);
   fill(  0 , 255 , 0 );
   background(255 , 0 , 0 , 40 );
 }
