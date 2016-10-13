ca1a34140ffb77489cbe7747bb95e20260b8d478
HippoFalcon <277467@bwls.net>
2016-10-13 16:46:26 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ function draw() {
   ellipse( mouseX , mouseY , mouseX , mouseY  );
   fill( 0 , 0 , 255 );
   noStroke();
-  triangle( mouseX+40 , mouseY+30 , mouseX-40 , mouseY+30, mouseX , mouseY-40);
+  triangle( mouseX+70 , mouseY+30 , mouseX-40 , mouseY+30, mouseX , mouseY-40);
   fill(  0 , 255 , 0 );
   background(255 , 0 , 0 , 40 );
 }
