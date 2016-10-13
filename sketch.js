4f3e2fa7fb4f9eb9d0ba097319af20c9e9ca3e05
HippoFalcon <277467@bwls.net>
2016-10-13 15:17:00 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 function setup() {
   createCanvas( 800 , 800 );
-  background(255 , 0 , 0)
+  background(255 , 0 , 0);
 }
 
 function draw() {
   ellipse( mouseX , mouseY , 100 , 100 );
-  fill( 0 , 255 , 0 ):
-  noStroke()
+  fill( 0 , 255 , 0 );
+  noStroke();
 }
