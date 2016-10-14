079e35cf7028ec13a963a38d1f7ad1ba07801bfd
Brendan Hoppe <277467@bwls.net>
2016-10-14 08:23:06 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,19 @@
 function setup() {
   createCanvas( windowWidth , windowHeight );
-    background(255 , 255 , 0 , 40 );
+    background(69 , 69 , 69 , 40 );
 
 }
 
 function draw() {
   ellipse( mouseX , mouseY , 21 , 21  );
-  fill( 104 , 0, 0 );
+  fill( 104 , 0, 0 , 40);
   noStroke();
   
   ellipse( mouseX , mouseY, 13 , 13 );
-  fill( 0 , 104 , 0 );
+  fill( 0 , 104 , 0 , 40);
   noStroke();
   
   ellipse(mouseX , mouseY , 7 , 7);
-  fill(0 , 0 , 104);
+  fill(0 , 0 , 104 , 40);
   noStroke();
 }
